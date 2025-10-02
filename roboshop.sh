14bf410626788ad4ed520d9ded4ec1b5114a46a2
balamuralich <jyobalach@gmail.com>
2025-10-02 11:58:39 +0530

    robo

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-03249e1d40b5de776"
+ZONE_ID="Z0881346IGZYG6U9FIPG"
+DOMAIN_NAME="jyobala.space"
 
 for INSTANCE in $@
 
@@ -10,8 +12,29 @@ do
 
     if [ $INSTANCE != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$INSTANCE.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
         echo "$INSTANCE; $IP"
+
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+        {
+            "Comment": "Creating a record set"
+            ,"Changes": [{
+            "Action"              : "UPSERT"
+            ,"ResourceRecordSet"  : {
+                "Name"              : "'$RECORD_NAME'"
+                ,"Type"             : "A"
+                ,"TTL"              : 1
+                ,"ResourceRecords"  : [{
+                    "Value"         : "'$IP'"
+                }]
+            }
+            }]
+        }
+        '
 done
