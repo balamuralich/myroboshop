65269e9d61e3ba15612232bb5a01217284342cf3
balamuralich <jyobalach@gmail.com>
2025-10-02 11:26:43 +0530

    robo

--- roboshop.sh
+++ roboshop.sh
@@ -4,13 +4,14 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-03249e1d40b5de776"
 
 for INSTANCE in $@
+
 do
     INSTANCE_ID$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" --query "Instances[0].InstanceId" --output text)
 
     if [ $INSTANCE !n "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIPAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIPAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
         echo "$INSTANCE; $IP"
 done
