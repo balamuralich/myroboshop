f2b2ff386007fb94b825a34b0cd6eccda0616d0a
Balamurali Krishna C <jyobala@Balamuralis-MacBook-Air.local>
2025-10-03 22:38:27 +0530

    New change

--- cart.sh
+++ cart.sh
@@ -69,8 +69,6 @@ VALIDATE $? "Removing existing code"
 unzip /tmp/cart.zip &>>Logs_file               #Unzips the new code.
 VALIDATE $? "Unzip Cart"
 
-cd /app
-
 npm install &>>Logs_file                            #Installs required Node.js packages from package.json.
 VALIDATE $? "Installing Dependencies"
 
