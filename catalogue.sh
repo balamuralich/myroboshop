3bec924d47dd7c4fd5ebbef56de1c7fcdd67dab4
balamuralich <jyobalach@gmail.com>
2025-10-03 07:14:31 +0530

    robo

--- catalogue.sh
+++ catalogue.sh
@@ -73,7 +73,7 @@ systemctl daemon-reload
 systemctl enable catalogue
 VALIDATE $? "Enabling Catalogue"
 
-cp /home/ec2-user/myroboshop/mango.repo/etc/yum.repos.d/mongo.repo
+cp $SCRIPT_DIR/mongo.repo/etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copy Mongo repo"
 
 dnf install mongodb-mongosh -y &>>Logs_file
